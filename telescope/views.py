871d28fccf7cd141b740d127038fe182e2930142
Junior Gantin <nioperas06@gmail.com>
2024-01-10 17:22:19 +0100

    Add pagination

--- telescope/views.py
+++ telescope/views.py
@@ -2,16 +2,23 @@ import json
 from django.shortcuts import render
 from telescope.models import Entry
 from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
     requests = Entry.objects.all()
 
-    paginator = Paginator(requests, 25)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+    paginator = Paginator(requests, 10)
+    page = request.GET.get("page", 1)
 
-    print(page_obj)
+    try:
+        paginated_items = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        paginated_items = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range, deliver last page of results.
+        paginated_items = paginator.page(paginator.num_pages)
 
     requests = [
         {
@@ -19,7 +26,7 @@ def index(request):
             'created_at': item.created_at.strftime('%Y-%m-%dT%H:%M:%S'),
             'content': json.loads(item.content),
         }
-        for item in requests
+        for item in paginated_items
     ]
 
-    return render(request, "requests/index.html", {'requests': json.dumps({'values': requests})})
+    return render(request, "requests/index.html", {'requests': json.dumps({'values': requests}), 'paginated_items': paginated_items})
